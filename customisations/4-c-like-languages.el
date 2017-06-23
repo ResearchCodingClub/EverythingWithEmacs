9fbc121d6080c2270355eb2a1d0d848ea4f15ff0
Peter Hill <zed.three@gmail.com>
2017-06-23 09:45:27 +0100

    Add more compilation/completion support for C

--- customisations/4-c-like-languages.el
+++ customisations/4-c-like-languages.el
@@ -24,3 +24,82 @@
   (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
   ;; Colour in shell?
   (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Compile customisation
+
+(use-package cl
+  ;; cl is a built-in package, so don't try and install it!
+  :ensure nil
+
+  :bind
+  (("<f5>" . recompile))
+
+  :init
+  (setq
+   compilation-scroll-output 'first-error   ;; scroll until first error
+   compilation-skip-threshold 2             ;; skip warnings
+   compilation-read-command t               ;; confirm compile command
+   compilation-window-height 12             ;; keep it readable
+   compilation-auto-jump-to-first-error nil ;; jump to first error auto
+   compilation-auto-jump-to-next-error nil  ;; jump to next error
+   )
+
+  ;; If there were no compilation errors, delete the compilation window
+  (setq compilation-exit-message-function
+        (lambda (status code msg)
+          ;; If M-x compile exits with a 0
+          (when (and (eq status 'exit) (zerop code))
+            ;; then bury the *compilation* buffer, so that C-x b doesn't go there
+            (bury-buffer "*compilation*")
+            ;; and return to whatever were looking at before
+            (replace-buffer-in-windows "*compilation*"))
+          ;; Always return the anticipated result of compilation-exit-message-function
+          (cons msg code))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Company mode for code completion
+
+(use-package company
+  :config
+  (global-company-mode)
+
+  :diminish company-mode)
+
+(use-package irony
+  :config
+  (use-package company-irony
+    :ensure t
+    :config
+    (push 'company-irony company-backends)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rtags - C++ aware tagging
+
+;; http://www.rtags.net
+
+(use-package rtags
+  :init
+  (setq rtags-path "~/Tools/rtags/install/bin")
+
+  ;; Start rtags automatically for C/C++
+  (add-hook 'c-mode-common-hook #'rtags-start-process-unless-running)
+
+  :config
+  (rtags-enable-standard-keybindings)
+
+  ;; Get completions working with company mode
+  (setq rtags-autostart-diagnostics t)
+  (rtags-diagnostics)
+  (setq rtags-completions-enabled t)
+  (push 'company-rtags company-backends)
+
+  ;; Need to have helm installed as well
+  (use-package helm-rtags
+    :init
+    (setq rtags-use-helm t)
+
+    :config
+    (setq rtags-display-result-backend 'helm))
+
+  (use-package flycheck-rtags))
