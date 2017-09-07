f4144d3372743a3a6335aa8de10c22b353240841
Peter Hill <zed.three@gmail.com>
2017-09-07 09:28:19 +0100

    Start expanding programming languages

--- customisations/6-python.el
+++ customisations/6-python.el
@@ -3,9 +3,12 @@
 
 (use-package elpy
   :config
-  (setq elpy-rpc-python-command "python3")
-  (setq elpy-test-runner 'elpy-test-pytest-runner)
+  ;; Use Python 3 for the backend
+  (setq elpy-rpc-python-command "python3"
+        ;; Use PyTest to run tests
+        elpy-test-runner 'elpy-test-pytest-runner)
 
+  ;; Use flycheck for syntax checking as you type
   (when (require 'flycheck nil t)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
     (add-hook 'elpy-mode-hook 'flycheck-mode)))
