79513772dc298b7146a985e56b282e81161e33db
Peter Hill <zed.three@gmail.com>
2017-09-08 13:46:25 +0100

    Add python section

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hello',
+      version='0.1.0',
+      description='Hello world',
+      packages=['hello'],
+      install_requires=['colorama'],
+      keywords=['build', 'dependencies', 'fortran'],
+)
